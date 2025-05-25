1c654364ddb9ed5cccf2a4d9ac3bc10c23b65477
Immanuel Dsouza <dsouzad681@gmail.com>
2025-05-26 00:08:49 +0530

    done with basics module of ICPC

--- Basics/PrbT.cxx
+++ Basics/PrbT.cxx
@@ -1,33 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+void print(string s, string key)
+{
+    int pos = s.find(key);
+    int pos1 = s.find("=", pos + 1);
+    int pos2 = s.find('&', pos1 + 1);
+
+    cout << s.substr(pos, pos1 - pos) << ": " << s.substr(pos1 + 1, pos2 - pos1 - 1) << endl;
+}
+
+int main()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    string s = "http://www.cleartrip.com/signin/service?username=test&pwd=test&profile=developer&role=ELITE&key=manager";
-
-    string temp="";
-    int i=0;
-    bool found = false;
-    for(i=0;i<s.size();i++) {
-        if(s[i]=='?'){
-            found = true;
-            
-            break;
-        }
-    }
-
-    bool inKey = true;
-    bool inValue = false;
-    if (found){
-        temp = s.substr(i+1);
-
-        for(int i=0;i<temp.size() ;i++) {
-            if()
-        }
-    }
-
+    string s ;
+    cin>>s;
+    print(s, "username");
+    print(s, "pwd");
+    print(s, "profile");
+    print(s, "role");
+    print(s, "key");
 
     return 0;
 }
